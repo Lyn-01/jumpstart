05312653208bfc2dc78c51cdfcba7696841cdede
Lyn-01 <30039003+Lyn-01@users.noreply.github.com>
2017-09-24 19:41:41 +0200

    final commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.carousel').slick({
-    setting-name: setting-value
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  var scrollDistance = 0;
+  $(window).scroll(function() {
+    var currentDistance = $(this).scrollTop();
+    if (currentDistance >= (scrollDistance + 50)) {
+      var navbarHeight = $('.navbar').height();
+      $('.navbar').animate({top: -1 * navbarHeight}, 150);
+      scrollDistance = currentDistance;
+    }else if (scrollDistance >= (currentDistance + 50)) {
+      $('.navbar').animate({top: "0px"}, 150);
+      scrollDistance = currentDistance;
+    }
   });
 });
-
-$('.single-item').slick();
